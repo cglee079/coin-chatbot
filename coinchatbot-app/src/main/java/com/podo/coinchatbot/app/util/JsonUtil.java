7f96f71ae810c7b58426c4a6c847ac9ca0be2dd8
podo.dev <podo.dev@kakaoenterprise.com>
2021-01-30 00:13:19 +0900

    feature : log change

---
+++ coinchatbot-app/src/main/java/com/podo/coinchatbot/app/util/JsonUtil.java
@@ -0,0 +1,46 @@
+package com.podo.coinchatbot.app.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.experimental.UtilityClass;
+
+import java.io.File;
+import java.io.IOException;
+
+@UtilityClass
+public class JsonUtil {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.registerModule(new JavaTimeModule());
+        OBJECT_MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    public static <T> String toJSON(T object) {
+        try {
+            return OBJECT_MAPPER.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("OBJECT MAPPER FAIL :: , " + object, e);
+        }
+    }
+
+    public static <T> T toObject(String value, Class<T> clazz) {
+        try {
+            return OBJECT_MAPPER.readValue(value, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("OBJECT MAPPER FAIL :: " + value, e);
+        }
+    }
+
+    public static <T> T toObject(File file, Class<T> clazz) {
+        try {
+            return OBJECT_MAPPER.readValue(file, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException("메시지 파싱 실패 !" + file.getAbsolutePath(), e);
+        }
+    }
+}
+
